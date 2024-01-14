b01bc8d0b39d7779bd7bf398df04baf1bb7bc912
ZHAYRI123 <merykozan@gmail.com>
2024-01-14 06:06:08 -0800

    do setup_mysql_test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,4 @@
+--A database hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+--A new user hbnb_test (in localhost)
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
