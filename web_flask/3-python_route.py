f923f021c954793d3d74633b78fcdb2523bef3bd
ZHAYRI123 <merykozan@gmail.com>
2024-03-24 21:29:02 +0000

    tasks 4: default value

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Simple Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Display the text 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def say_hbnb():
+    """Display the text 'HBNB'"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_c(text):
+    """Display 'C' followed by the text variable."""
+    t = text.replace("_", " ")
+    return "C {}".format(t)
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """Route that displays 'Python ' followed by the value of the text variable."""
+    return 'Python {}'.format(escape(text.replace('_', ' ')))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
